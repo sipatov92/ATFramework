e64c4ff5a27b8555a3f4f2d08f414aa634a9edf2
alexbeatnik <alexbeatnik@gmail.com>
2021-08-03 10:43:59 +0300

    Add Test1

---
+++ ATFramework/Test1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace ATFramework
+{
+    public class Test1
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(@"http://localhost:5000/");
+            driver.FindElement(By.XPath("//a[@href='/add_remove_elements/']")).Click();
+            driver.FindElement(By.XPath("//button[@onclick='addElement()']")).Click();
+            var buttonDelete = driver.FindElement(By.XPath("//button[@onclick='deleteElement()']"));
+            Assert.True(buttonDelete.Displayed, "Button Delete is not Displayed");
+            buttonDelete.Click();
+            try
+            {
+                Assert.False(buttonDelete.Displayed, "Button Delete is Displayed");
+            }
+            catch (StaleElementReferenceException e)
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
