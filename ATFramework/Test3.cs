00052a70454500d347dc0f27dbd63260bc14b4d3
alexbeatnik <alexbeatnik@gmail.com>
2021-08-03 10:44:21 +0300

    Add Test3

---
+++ ATFramework/Test3.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace ATFramework
+{
+    public class Test3
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(@"http://localhost:5000/");
+            driver.FindElement(By.XPath("//a[@href='/key_presses']")).Click();
+            var editbox = driver.FindElement(By.Id("target"));
+            editbox.SendKeys("a");
+            var result = driver.FindElement(By.Id("result"));
+            Assert.True(result.Text.Equals("You entered: A"));
+            editbox.SendKeys("b");
+            Assert.True(result.Text.Equals("You entered: B"));
+            driver.Quit();
+        }
+    }
+}
